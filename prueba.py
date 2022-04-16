f278aef45fde27a84b683bd687e4999dbefb40f4
Luis Mogro <luismogro65@gmail.com>
2022-04-16 17:59:56 -0300

    comienzo del proyecto

---
+++ prueba.py
@@ -0,0 +1,21 @@
+cadena1='hello, world'
+print(cadena1)
+perro=cadena1.replace( ', ' , ' : ')
+print(perro)
+
+
+aux=7
+cadena='A7:E7' 
+while aux<=10:
+     baux=cadena.replace('7',str(aux))
+     cadena3 = sheet_ranges[{baux}]
+     aux=aux+1
+
+ 
+
+
+
+text = "Apples taste Good."
+print(text)
+gato=text.replace('Apples', 'Bananas')
+print(gato)
